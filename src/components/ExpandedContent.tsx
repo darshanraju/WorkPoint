1236309dc8144c3fa47d0bf1905adc211cc4aa86
Darshan <darshan.raju99@gmail.com>
2022-09-07 09:00:09 +1000

    show apply on mobile

--- src/components/ExpandedContent.tsx
+++ src/components/ExpandedContent.tsx
@@ -3,9 +3,11 @@ import { IJobAdd, Section, List } from "./JobAdd";
 
 const ExpandedContent = (ctx: IJobAdd) => {
   return (
-    <div className="p-10">
-      <div className="text-2xl text-start">{ctx.company} is hiring a</div>
-      <div className="text-3xl text-start">{ctx.role}</div>
+    <div className="p-5 lg:p-10">
+      <div className="text-lg lg:text-2xl text-start">
+        {ctx.company} is hiring a
+      </div>
+      <div className="text-xl lg:text-3xl text-start">{ctx.role}</div>
       {ctx.jobInfo?.preText?.map((section, idx) => {
         return <Section text={section.text} title={section.title} key={idx} />;
       })}
@@ -15,6 +17,14 @@ const ExpandedContent = (ctx: IJobAdd) => {
       {ctx.jobInfo?.postText?.map((section, idx) => {
         return <Section text={section.text} title={section.title} key={idx} />;
       })}
+      <div className=" items-center lg:hidden" onClick={() => 42}>
+        <button
+          className="btn btn-secondary"
+          onClick={() => window.open(ctx.link, "_blank")}
+        >
+          Apply
+        </button>
+      </div>
     </div>
   );
 };
